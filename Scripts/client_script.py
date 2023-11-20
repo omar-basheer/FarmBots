fa0679d8b0c875671e2d8c1b586ce5bcf74dd339
omar-basheer <112007297+omar-basheer@users.noreply.github.com>
2023-11-20 20:41:45 +0000

    update test_handshake, handshake, client_script, server_script, and localization.py

--- Scripts/client_script.py
+++ Scripts/client_script.py
@@ -1,4 +1,4 @@
-from WirelessComm.communications import connect_to_bluetooth_server, create_mailbox, close_bluetooth_connection, test_handshake
+from WirelessComm.communications import connect_to_bluetooth_server, create_mailbox, close_bluetooth_connection, handshake, test_handshake
 
 def main():
     server_brick_address = 'ash-ev3-07'
@@ -7,24 +7,9 @@ def main():
     client = connect_to_bluetooth_server(server_brick_address)
 
     # Test handshake
-    test_handshake(None, client, server_mailbox_name='handshake', client_mailbox_name='handshake')
+    test_handshake(None, client, server_mailbox_name='client1', client_mailbox_name='client1', client_num=0)
+    handshake(None, client, server_mailbox_name='client1', client_mailbox_name='client1', client_num=0)
 
-    
-    # server_brick_address = 'ash-ev3-07'
-
-    # #connect to bluetooth server on Server ev3
-    # client = connect_to_bluetooth_server(server_brick_address)
-
-    # #create a dedicated mailbox on the client
-    # client_mailbox_name = 'handshake'
-    # client_mailbox = create_mailbox(client_mailbox_name, client)
-
-    # #send message to server
-    # message_to_send = "hello, server!"
-    # client_mailbox.send(message_to_send)
-
-    # #close the connection
-    # close_bluetooth_connection(client)
 
 if __name__ == "__main__":
     main()
--- WirelessComm/communications.py
+++ WirelessComm/communications.py
@@ -1,5 +1,7 @@
+from Localization.localization import get_current_position
 from pybricks.messaging import BluetoothMailboxServer, BluetoothMailboxClient, TextMailbox, Mailbox
 from pybricks import ev3brick as brick
+import time
 
 def start_bluetooth_server(client_number):
     server = BluetoothMailboxServer()
@@ -30,29 +32,74 @@ def send_message_to_server(mailbox, message):
     except OSError as e:
         print(f"Error sending message to server: {e}")
 
+def send_position_updates(client_mailbox, client_id):
+    for i in range(10):
+        x, y, theta = get_current_position()
 
-def test_handshake(server, client, server_mailbox_name, client_mailbox_name):
-    #create dedicated mailbox on server
-    server_mailbox = create_mailbox(server_mailbox_name, server)
+        # Include client identifier in the position update message
+        message_to_send = f"Position Update from {client_id}: X={x}, Y={y}, Theta={theta}"
+        client_mailbox.send(message_to_send)
+
+        # Wait for a short interval before sending the next update
+        time.sleep(1)
+
+def test_handshake(server, client, server_mailbox_name, client_mailbox_name, client_num):
+    for i in range(client_num):
+        #create dedicated mailbox on server
+        server_mailbox = create_mailbox(server_mailbox_name + str(i), server)
 
     #wait for client(s) to connect
     print("waiting for client(s) to connect...")
     server.wait_for_connection()
-
-    #receive message from client
-    received_message = server_mailbox.read()
-    print(f"Received message from client: {received_message}")
+    print('connected!')
 
     #create a dedicated mailbox on the client
     client_mailbox = create_mailbox(client_mailbox_name, client)
 
     #send a message to server
-    message_to_send = "hello, server!"
+    # client_id = brick.bluetooth.name
+    message_to_send = "hello, server! This is " + client_mailbox_name
     client_mailbox.send(message_to_send)
+
+    #receive message from client
+    received_message = server_mailbox.read()
+    print(f"Received message from client: {received_message}")
 
     # close connections
     close_bluetooth_connection(server)
     close_bluetooth_connection(client)
+
+def handshake(server, client, server_mailbox_name, client_mailbox_name, client_num):
+    for i in range(client_num):
+        #create dedicated mailbox on server
+        server_mailbox = create_mailbox(server_mailbox_name + str(i), server)
+
+    #wait for client(s) to connect
+    print("waiting for client(s) to connect...")
+    server.wait_for_connection()
+    print('connected!')
+
+    #create a dedicated mailbox on the client
+    client_mailbox = create_mailbox(client_mailbox_name, client)
+
+    # Send position updates from the client to the server
+    # send_position_updates(client_mailbox, client_id)
+    for i in range(10):
+        x, y, theta = get_current_position()
+
+        # Include client identifier in the position update message
+        message_to_send = f"Position Update from {client_mailbox_name}: X={x}, Y={y}, Theta={theta}"
+        client_mailbox.send(message_to_send)
+
+        #receive message from client
+        received_message = server_mailbox.read()
+        print(f"Received message from client: {received_message}")
+
+        # Wait for a short interval before sending the next update
+        time.sleep(1)
+
+
+
 
 
 
